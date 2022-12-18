270c0d032c6c22126ff8a64d39ebdd51569ec5a9
nabin <jahangirnabin2@gmail.com>
2022-12-18 16:29:13 +0600

    update

--- js/index.js
+++ js/index.js
@@ -89,6 +89,16 @@ function reveal() {
       }
     }
   }
+
+  let isInViewport = function (elem) {
+    var bounding = elem.getBoundingClientRect();
+    return (
+        bounding.top >= 0 &&
+        bounding.left >= 0 &&
+        bounding.bottom <= (window.innerHeight || document.documentElement.clientHeight) &&
+        bounding.right <= (window.innerWidth || document.documentElement.clientWidth)
+    );
+  };
   
   window.addEventListener("scroll", reveal);
   
